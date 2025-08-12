5edbb05d50339e4c1065d7b07981e59d146ed96c
Danrley Lazaro Maganha <33334455+dallaz@users.noreply.github.com>
2025-08-12 17:16:50 -0300

    Initial commit
    Initial commit

---
+++ src/main/java/com/sistema/yokota/YokotaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.yokota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokotaApplication.class, args);
+	}
+
+}
